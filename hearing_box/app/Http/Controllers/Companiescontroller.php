df76eadbd85af716701f7de37132ab3eaf9fb0e8
koki <k.blog2019@gmail.com>
2019-06-24 22:07:16 +0900

    CompaniescontrollerをCompaniesControllerに名前変更

--- hearing_box/app/Http/Controllers/Companiescontroller.php
+++ hearing_box/app/Http/Controllers/Companiescontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class Companiescontroller extends Controller
+class CompaniesController extends Controller
 {
     public function index(Request $request)
     {
