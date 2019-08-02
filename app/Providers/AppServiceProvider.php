e48464defae5ca1fb16bd9dbab00c433c1fb13bf
koki <k.blog2019@gmail.com>
2019-08-02 21:46:52 +0900

    AppServiceProvider.phpの修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //// 本番環境(Heroku)でhttpsを強制する
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+
     }
-}
 }
