e3f8d8c5090dabcf288dfbde5d3abb836fdc4db2
koki <k.blog2019@gmail.com>
2019-08-06 22:47:46 +0900

    質問編集画面の日付選択修正

--- resources/views/sheets/sheets_edit.blade.php
+++ resources/views/sheets/sheets_edit.blade.php
@@ -13,9 +13,9 @@
     <form action="/sheets/{{$sheets->id}}/edit" method="POST">
         {{ csrf_field() }}
         <div class="form-group">
-        <p>タイトル・訪問日の編集</p>
-        <textarea class="form-control" name="title" value="{{old('title')}}" rows="1">{{$sheets->title}}</textarea>
-        <textarea class="form-control" name="visit_date" value="{{old('visit_date')}}" rows="1">{{$sheets->visit_date}}</textarea>
+            <p>タイトル・訪問日の編集</p>
+            <textarea class="form-control" name="title" value="{{old('title')}}" rows="1">{{$sheets->title}}</textarea>
+            <input type="date" name="visit_date" value="{{old('visit_date')}}">
         </div>
         <input type="submit" class="btn btn-primary" name="edit" value="保存">
     </form>
